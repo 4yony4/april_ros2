e6667d9606f97a8b2bcdcdc893868fa8e109ad22
Yony Bar-Magen <jonathan.barmagen@iit.it>
2022-04-27 19:30:08 +0200

    New Dummy Structure

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
+from glob import glob
 from setuptools import setup
+
 
 package_name = 'april_ros2'
 
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +24,13 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'dummy = april_ros2.dummy:main',
+            #'dummy = april_ros2.dummy:main',
+            'dummy_grasp = april_ros2.dummy_grasp:main',
+            'dummy_fa = april_ros2.dummy_fa:main',
+            'dummy_hb = april_ros2.dummy_hb:main',
+            'dummy_nc = april_ros2.dummy_nc:main',
+            'dummy_nv = april_ros2.dummy_nv:main',
+            
             'sim = april_ros2.sim:main',
             'sem = april_ros2.sem:main',
         ],
