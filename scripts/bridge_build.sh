6244851f34bc3148250b86da79ce855f2914907f
Jonathan Z. Bar-Magen Numhauser <jonathan.barmagen@iit.it>
2022-07-21 16:37:55 +0200

    latest updates

--- scripts/bridge_build.sh
+++ scripts/bridge_build.sh
@@ -4,6 +4,8 @@ ros2_dir=~/ros2_galactic/
 export ROS1_INSTALL_PATH=/opt/ros/noetic
 export ROS2_INSTALL_PATH=$ros2_dir/install
 
+colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure
+
 #ROS1 Environment initialization
 source ${ROS1_INSTALL_PATH}/setup.bash
 
@@ -14,10 +16,10 @@ source ${ROS2_INSTALL_PATH}/setup.bash
 
 #Overlay workspaces
 source ~/Dev/ROS/april_ros/devel/setup.bash
-source ~/Dev/ROS2/april_ros2/src/install/setup.bash
-
+source ~/Dev/ROS2/april_ros2/install/local_setup.bash
+source ~/Dev/ROS2/april_msgs/install/local_setup.bash
 
 #RUN BUILD
 cd $ros2_dir
 colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure
-cd $current_dir
+cd ~/Dev/ROS2/april_ros2/scripts/
---
+++ scripts/nc_driver_run.sh
@@ -0,0 +1,5 @@
+cd ..
+. install/local_setup.bash
+ros2 launch metavision_ros_driver driver_node.launch.py
+#ros2 launch metavision_ros_driver driver_composition.launch.py
+cd scripts
