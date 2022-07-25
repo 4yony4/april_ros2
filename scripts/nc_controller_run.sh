eae0628ce8fe13a95da186b0d77aa67e9a5c5dcf
Jonathan Z. Bar-Magen Numhauser <jonathan.barmagen@iit.it>
2022-07-25 09:56:14 +0200

    Added event_ros_tools slicer to visualize image events
    Former-commit-id: e8fed28c091125cdc98d3eff4ee2fcb245d9f774

--- scripts/nc_controller_run.sh
+++ scripts/nc_controller_run.sh
@@ -1,14 +1,16 @@
 
 ros2_dir=~/ros2_galactic/
 
-source $ros2_dir/install/setup.bash
+#source $ros2_dir/install/setup.bash
 
 cd ..
 
+#colcon build --packages-select event_ros_tools
 colcon build --packages-select nc_controller
 
-source install/setup.bash
+source install/local_setup.bash
 
-cd ./scripts/
 
 ros2 run nc_controller nc_main
+
+cd ./scripts/
