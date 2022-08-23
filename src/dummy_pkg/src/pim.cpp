4d0ef1fe31b782b777502a01d0893a2ba1eaec51
Jonathan Z. Bar-Magen Numhauser <jonathan.barmagen@iit.it>
2022-08-23 12:37:02 +0200

    added all basic dummy publication of topics in the dummy package, still have to add other combinations
    Former-commit-id: 22f2bd0bd88f04c96a18eeee1271498cdb664963

--- src/dummy_pkg/src/pim.cpp
+++ src/dummy_pkg/src/pim.cpp
@@ -1,10 +1,70 @@
+#include <string>
 #include <cstdio>
+
+#include "rclcpp/rclcpp.hpp"
+#include "april_msgs/msg/robot_arm.hpp"
+#include "april_msgs/msg/robot_hand.hpp"
+#include "april_msgs/msg/tactile.hpp"
+
+static const std::string TOPIC_ROBOT_ARM = "/pim/robot_arm";
+static const std::string TOPIC_ROBOT_HAND = "/pim/robot_hand";
+static const std::string TOPIC_TACTILE = "/pim/tactile";
+
+class pim : public rclcpp::Node
+{
+
+  public:
+    pim()
+    : Node("pim")
+    {
+      publisher_robot_arm = this->create_publisher<april_msgs::msg::RobotArm>(TOPIC_ROBOT_ARM, 10);
+      publisher_robot_hand = this->create_publisher<april_msgs::msg::RobotHand>(TOPIC_ROBOT_HAND, 10);
+      publisher_tactile = this->create_publisher<april_msgs::msg::Tactile>(TOPIC_TACTILE, 10);
+    }
+
+  private:
+
+  rclcpp::Publisher<april_msgs::msg::RobotArm>::SharedPtr publisher_robot_arm;
+  rclcpp::Publisher<april_msgs::msg::RobotHand>::SharedPtr publisher_robot_hand;
+  rclcpp::Publisher<april_msgs::msg::Tactile>::SharedPtr publisher_tactile;
+
+  void publish_robot_arm() const{
+    april_msgs::msg::RobotArm robot_arm = april_msgs::msg::RobotArm();
+    
+    robot_arm.set__state("ON");
+    
+    if(publisher_robot_arm->get_subscription_count() !=0){
+      publisher_robot_arm->publish(robot_arm);
+    }
+  }
+
+  void publish_robot_hand() const{
+    april_msgs::msg::RobotHand robot_hand = april_msgs::msg::RobotHand();
+    
+    robot_hand.set__state("ON");
+    
+    if(publisher_robot_hand->get_subscription_count() !=0){
+      publisher_robot_hand->publish(robot_hand);
+    }
+  }
+
+  void publish_tactile() const{
+    april_msgs::msg::Tactile tactile = april_msgs::msg::Tactile();
+    
+    tactile.set__label("label1");
+    tactile.set__temperature(45.3);
+    
+    if(publisher_tactile->get_subscription_count() !=0){
+      publisher_tactile->publish(tactile);
+    }
+  }
+
+};
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world PIM node dummy_pkg package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<pim>());
+  rclcpp::shutdown();
   return 0;
 }
