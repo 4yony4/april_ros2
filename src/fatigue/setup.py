5458525d551e81ba3069742289b3ed3390d39fc1
Jonathan Z. Bar-Magen Numhauser <jonathan.barmagen@iit.it>
2022-08-25 12:33:13 +0200

    adding facial experision recognition into fatigue
    Former-commit-id: 8e98de23a51b33ab5107dd47ad9e740cc9dd1f00

--- src/fatigue/setup.py
+++ src/fatigue/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'dummy_fa = fatigue.dummy_fa:main',
+            #'dummy_fa = fatigue.dummy_fa:main',
+            'detection = fatigue.detection:main',
         ],
     },
 )
