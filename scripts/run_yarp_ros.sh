81af7f063b85f21847cf6e643b3a4de5f8b4cead
Jonathan Z. Bar-Magen Numhauser <jonathan.barmagen@iit.it>
2022-09-07 22:11:12 +0200

    finished communication between YARP->ROS1->ROS2 using bridges and a test package called yarp_com_pkg, added all code to the script run_yarp_ros
    Former-commit-id: cb29cd450ce5235eeebd90d45a9e15450f987a6d

--- scripts/run_yarp_ros.sh
+++ scripts/run_yarp_ros.sh
@@ -1,8 +1,16 @@
-#START YARP SERVER
-gnome-terminal --tab -- bash -c "yarpserver --ros; exec bash -i"
-
 #START ROSCORE (ROS1 SERVER)
-gnome-terminal --tab -- bash -c "source /opt/ros/noetic/setup.bash; roscore; exec bash -i"
+gnome-terminal --title="ROSCORE ROS1 Server" --tab -- bash -c "source /opt/ros/noetic/setup.bash; roscore; exec bash -i"
+
+#START YARP SERVER
+gnome-terminal --title="YARP SERVER" --tab -- bash -c "export ROS_MASTER_URI=http://localhost:11311; yarpserver --ros; exec bash -i"
 
 #START ROS1 <---> ROS2 BRIDGE
-gnome-terminal --tab -- bash -c "source run_bridge.sh; exec bash -i"
+gnome-terminal --title="ROS1 <--> ROS2 Bridge" --tab -- bash -c "source run_bridge.sh; exec bash -i"
+
+#START YARP TALKER
+gnome-terminal --title="YARP TALKER" --tab -- bash -c "sleep 2; cd ~/Dev/yarp/talker/;./talker; exec bash -i"
+
+#START YARP TALKER
+gnome-terminal --title="APRIL ROS2 RECEIVER" --tab -- bash -c "sleep 2; cd ~/Dev/ROS2/april_ros2;ros2 run yarp_com_pkg yarp_com; exec bash -i"
+
+
